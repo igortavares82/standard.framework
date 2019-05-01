eb10eb5e43ec49fa94045f65f201dd7d503e1dc4
IGORTB\Igor Tavares Barata <igor.meier@gmail.com>
2019-05-01 17:45:29 -0300

    no message

---
+++ Stone.Framework.Utils/Cpf/CpfGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Stone.Framework.Utils.Cpf
+{
+    public class CpfGenerator
+    {
+        public static string Generate()
+        {
+            int sum = 0, mod = 0;
+            int[] mt1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] mt2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            Random rnd = new Random();
+            string seed = rnd.Next(100000000, 999999999).ToString();
+
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(seed[i].ToString()) * mt1[i];
+
+            mod = sum % 11;
+            if (mod < 2)
+                mod = 0;
+            else
+                mod = 11 - mod;
+
+            seed = seed + mod;
+            sum = 0;
+
+            for (int i = 0; i < 10; i++)
+                sum += int.Parse(seed[i].ToString()) * mt2[i];
+
+            mod = sum % 11;
+
+            if (mod < 2)
+                mod = 0;
+            else
+                mod = 11 - mod;
+
+            seed = seed + mod;
+            return seed;
+        }
+    }
+}
