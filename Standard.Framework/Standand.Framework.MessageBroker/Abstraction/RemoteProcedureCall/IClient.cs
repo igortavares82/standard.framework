668b826e89d4270273f5d47970f1a8cc1ce2377a
Igor Tavares Baarata <igor.meier@gmail.com>
2020-07-27 10:50:39 -0300

    Desenvolvimento da comunicação sincrona

---
+++ Standard.Framework/Standand.Framework.MessageBroker/Abstraction/RemoteProcedureCall/IClient.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Standand.Framework.MessageBroker.Concrete.Options;
+using Standard.Framework.Seedworks.Concrete.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standand.Framework.MessageBroker.Abstraction.RemoteProcedureCall
+{
+    /// <summary>
+    /// Componente responsável por realizar chamadas síncronas a um determinado servidor.
+    /// </summary>
+    public interface IClient
+    {
+        /// <summary>
+        /// Executar uma chamada ao servidor em duas vias, com retorno conforme tipo esperado.
+        /// </summary>
+        /// <typeparam name="TRequest">Tipo da mensagem a ser enviada.</typeparam>
+        /// <typeparam name="TResponse">Tipo da mensagem a ser retornada.</typeparam>
+        /// <param name="request">Mensagem de retorno.</param>
+        /// <param name="context">Contexto do injetor de dependência.</param>
+        /// <param name="options">Opções de chamada.</param>
+        /// <returns>Retorno da chamada</returns>
+        Task<TResponseEvent> CallAsync<TRequestEvent, TResponseEvent>(TRequestEvent request, IComponentContext context, QueueOptions options) where TRequestEvent : IntegrationEvent
+                                                                                                                                              where TResponseEvent : IntegrationEvent
+    }
+}
---
+++ Standard.Framework/Standand.Framework.MessageBroker/Concrete/RemoteProcedureCall/Client.cs
@@ -0,0 +1,77 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Standand.Framework.MessageBroker.Abstraction;
+using Standand.Framework.MessageBroker.Abstraction.RemoteProcedureCall;
+using Standand.Framework.MessageBroker.Concrete.Options;
+using Standand.Framework.MessageBroker.Concrete.Serializers;
+using Standard.Framework.Seedworks.Concrete.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Standand.Framework.MessageBroker.Concrete.RemoteProcedureCall
+{
+    public class Client : Broker, IClient
+    {
+        public Client(IOptions<BrokerOptions> brokerOptions, ConnectionFactory factory, IConnection connection, IModel channel) : base(brokerOptions, factory, connection, channel)
+        {
+            Init();
+        }
+
+        public async Task<TResponseEvent> CallAsync<TRequestEvent, TResponseEvent>(TRequestEvent request, IComponentContext context, QueueOptions options) where TRequestEvent : IntegrationEvent
+                                                                                                                                                           where TResponseEvent : IntegrationEvent
+        {
+            TResponseEvent response = null;
+
+            MessageSerializer serializaer = new MessageSerializer();
+            IConfiguration configuration = context.Resolve<IConfiguration>();
+
+            byte[] byteRequest = serializaer.Serialize(request)[0];
+            byte[] byteResponse = await CallAsync(byteRequest, options);
+
+            response = serializaer.Deserialize<TResponseEvent>(byteResponse)[0];
+
+            return response;
+        }
+
+        private async Task<byte[]> CallAsync(byte[] message, QueueOptions options)
+        {
+            byte[] response = null;
+
+            if (options == null)
+                throw new ArgumentNullException("Queue configuration parameter cannot be null.");
+
+            byte[] result = new byte[0];
+
+            string replyQueue = string.Empty;
+            BlockingCollection<byte[]> respQueue = new BlockingCollection<byte[]>();
+
+            string correlationId = Guid.NewGuid().ToString();
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(Channel);
+            IBasicProperties props = Channel.CreateBasicProperties();
+
+            props.CorrelationId = correlationId;
+            replyQueue = props.ReplyTo = Channel.QueueDeclare().QueueName;
+
+            consumer.Received += (model, args) =>
+            {
+                if (args.BasicProperties.CorrelationId == correlationId)
+                    respQueue.Add(args.Body.ToArray());
+            };
+
+            await Task.Run(() =>
+            {
+                Channel.BasicPublish("", options.Queue, props, message);
+                Channel.BasicConsume(replyQueue, false, consumer);
+            });
+
+            response = respQueue.Take();
+
+            return response;
+        }
+    }
+}
