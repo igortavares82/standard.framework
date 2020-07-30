0413708c5d07325141ef156549a036426b6d4507
Igor Tavares Baarata <igor.meier@gmail.com>
2020-07-30 20:01:07 -0300

    Desenvolvimento do client e server

---
+++ Standard.Framework/Standand.Framework.MessageBroker/Abstraction/RemoteProcedureCall/IServer.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Standand.Framework.MessageBroker.Concrete.Options;
+using Standard.Framework.Seedworks.Concrete.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Standand.Framework.MessageBroker.Abstraction.RemoteProcedureCall
+{
+    public interface IServer
+    {
+        /// <summary>
+        /// Recebe chamadas do cliente e retorna uma resposta, sem receber parâmetro.
+        /// </summary>
+        /// <typeparam name="TRequest">Tipo da mensagem de requisição.</typeparam>
+        /// <typeparam name="TResponse">Tipo da mensagem de resposta.</typeparam>
+        /// <typeparam name="TIntegrationEventHandler">Tipo de tratamento do evento.</typeparam>
+        /// <param name="context">Contexto raiz do injetor dse dependência.</param>
+        /// <param name="configureScope">Configuração de escopo de objetos que deverão ser injetados.</param>
+        /// <param name="options"~>Opções de fila.</param>
+        /// <returns>Mensagem de retorno.</returns>
+        Task CallHandlerAsync<TRequest, TResponse, TIntegrationEventHandler>(IComponentContext context,
+                                                                             Action<ContainerBuilder, IConfiguration> configureScope,
+                                                                             QueueOptions options) where TRequest : IntegrationEvent
+                                                                                                   where TResponse : IntegrationEvent;
+
+        /// <summary>
+        /// Recebe chamadas do cliente e retorna uma resposta, sem receber parâmetro.
+        /// </summary>
+        /// <typeparam name="TResponse">Tipo da mensagem de resposta.</typeparam>
+        /// <param name="context">Contexto raiz do injetor dse dependência.</param>
+        /// <param name="configureScope">Configuração de escopo de objetos que deverão ser injetados.</param>
+        /// <param name="options">Opções de fila.</param>
+        /// <returns>Mensagem de retorno.</returns>
+        Task CallHandlerAsync<TResponse>(IComponentContext context,
+                                         Action<ContainerBuilder, IConfiguration> configureScope,
+                                         QueueOptions options) where TResponse : IntegrationEvent;
+    }
+}
---
+++ Standard.Framework/Standand.Framework.MessageBroker/Concrete/RemoteProcedureCall/Server.cs
@@ -0,0 +1,95 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Standand.Framework.MessageBroker.Abstraction;
+using Standand.Framework.MessageBroker.Abstraction.RemoteProcedureCall;
+using Standand.Framework.MessageBroker.Concrete.Options;
+using Standand.Framework.MessageBroker.Concrete.Serializers;
+using Standard.Framework.Seedworks.Abstraction.Events;
+using Standard.Framework.Seedworks.Concrete.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standand.Framework.MessageBroker.Concrete.RemoteProcedureCall
+{
+    public class Server : Broker, IServer
+    {
+        public Server(IOptions<BrokerOptions> brokerOptions, ConnectionFactory factory, IConnection connection, IModel channel) : base(brokerOptions, factory, connection, channel) { }
+
+        public async Task CallHandlerAsync<TRequest, TResponse, TIntegrationEventHandler>(IComponentContext context, 
+                                                                                    Action<ContainerBuilder, IConfiguration> configureScope, 
+                                                                                    QueueOptions options) where TRequest : IntegrationEvent
+                                                                                                          where TResponse : IntegrationEvent
+        {
+            EventingBasicConsumer consumer = BuildChannel(options);
+
+            consumer.Received += async (model, args) =>
+            {
+                TRequest request = null;
+                ILifetimeScope rootScope = context.Resolve<ILifetimeScope>();
+                IConfiguration configuration = rootScope.Resolve<IConfiguration>();
+
+                using (ILifetimeScope innerScope = rootScope.BeginLifetimeScope(Guid.NewGuid().ToString(), (config) => configureScope(config, context.Resolve<IConfiguration>())))
+                {
+                    MessageSerializer serializaer = new MessageSerializer();
+                    IBasicProperties props = args.BasicProperties;
+                    IBasicProperties replyProps = Channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    request = Activator.CreateInstance<TRequest>();
+
+                    if (args.Body.Length > 0)
+                        request = serializaer.Deserialize<TRequest>(args.Body.ToArray())[0];
+
+                    IIntegrationEventHandler<TRequest, TResponse> handler = innerScope.Resolve<IIntegrationEventHandler<TRequest, TResponse>>();
+                    TResponse response = await handler.Handle(request);
+
+                    Channel.BasicPublish("", props.ReplyTo, replyProps, serializaer.Serialize(response)[0]);
+                    Channel.BasicAck(args.DeliveryTag, false);
+                }
+            };
+        }
+
+        public async Task CallHandlerAsync<TResponse>(IComponentContext context, 
+                                                      Action<ContainerBuilder, IConfiguration> configureScope, 
+                                                      QueueOptions options) where TResponse : IntegrationEvent
+        {
+            EventingBasicConsumer consumer = BuildChannel(options);
+            ILifetimeScope rootScope = context.Resolve<ILifetimeScope>();
+            IConfiguration configuration = rootScope.Resolve<IConfiguration>();
+
+            consumer.Received += async (model, args) =>
+            {
+                using (ILifetimeScope innerScope = rootScope.BeginLifetimeScope(Guid.NewGuid().ToString(), (config) => configureScope(config, context.Resolve<IConfiguration>())))
+                {
+                    MessageSerializer serializaer = new MessageSerializer();
+                    IBasicProperties props = args.BasicProperties;
+                    IBasicProperties replyProps = Channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    IIntegrationEventHandler<TResponse> handler = innerScope.Resolve<IIntegrationEventHandler<TResponse>>();
+                    TResponse response = await handler.Handle(null);
+                    string json = JsonConvert.SerializeObject(response);
+
+                    Channel.BasicPublish("", props.ReplyTo, replyProps, Encoding.UTF8.GetBytes(json));
+                    Channel.BasicAck(args.DeliveryTag, false);
+                }
+            };
+        }
+
+        private EventingBasicConsumer BuildChannel(QueueOptions options)
+        {
+            EventingBasicConsumer consumer = new EventingBasicConsumer(Channel);
+
+            Channel.QueueDeclare(options.Queue, options.Durable, options.Exclusive, options.AutoDelete, null);
+            Channel.BasicQos(0, 1, false);
+            Channel.BasicConsume(options.Queue, false, consumer);
+
+            return consumer;
+        }
+    }
+}
